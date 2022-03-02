3bd2b186814d467412fc888fa2bd2d9d8e17954e
SheilaNS <shei.nsantos@gmail.com>
2022-03-02 19:44:21 -0300

    Header feito

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../assets/Header.css';
+
+class Header extends Component {
+  render() {
+    return (
+      <header>
+        <h1>Casa de Cambio</h1>
+      </header>
+    );
+  }
+}
+
+export default Header;
